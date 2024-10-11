1775b0172d2dfac7c380a25e12bc59ad1ca69037
Lucas Kook <lucasheinrich.kook@gmail.com>
2024-10-11 10:21:15 +0200

    add gcm test from pre-computed residuals

--- R/gcm.R
+++ R/gcm.R
@@ -68,8 +68,7 @@ gcm <- function(
     reg_YonZ = "rf", reg_XonZ = "rf", args_YonZ = NULL,
     args_XonZ = NULL, type = c("quadratic", "max", "scalar"), B = 499L,
     coin = TRUE, cointrol = list(distribution = "asymptotic"),
-    return_fitted_models = FALSE, ...
-) {
+    return_fitted_models = FALSE, ...) {
   Y <- .check_data(Y, "Y")
   X <- .check_data(X, "X")
   Z <- .check_data(Z, "Z")
@@ -91,7 +90,9 @@ gcm <- function(
 
   if (coin | NCOL(rY) > 1) {
     tst <- do.call("independence_test", c(list(
-      rY ~ rX, alternative = alternative, teststat = type), cointrol))
+      rY ~ rX,
+      alternative = alternative, teststat = type
+    ), cointrol))
     df <- NCOL(rY) * NCOL(rX)
     stat <- coin::statistic(tst)
     pval <- coin::pvalue(tst)
@@ -115,7 +116,9 @@ gcm <- function(
 
   models <- if (return_fitted_models) {
     list(reg_YonZ = mY, reg_XonZ = mX)
-  } else NULL
+  } else {
+    NULL
+  }
 
   structure(list(
     statistic = stat, p.value = pval, parameter = par,
@@ -123,8 +126,8 @@ gcm <- function(
     null.value = c("E[cov(Y, X | Z)]" = "0"), alternative = alternative,
     method = paste0("Generalized covariance measure test"),
     data.name = deparse(match.call(), width.cutoff = 80),
-    rY = rY, rX = rX, models = models), class = c("gcm", "htest"))
-
+    rY = rY, rX = rX, models = models
+  ), class = c("gcm", "htest"))
 }
 
 # Helpers -----------------------------------------------------------------
@@ -146,8 +149,9 @@ gcm <- function(
 }
 
 .compute_residuals <- function(y, pred) {
-  if (is.factor(y) && length(levels(y)) == 2)
+  if (is.factor(y) && length(levels(y)) == 2) {
     y <- as.numeric(y) - 1
+  }
   y - pred
 }
 
@@ -157,39 +161,44 @@ gcm <- function(
     N <- NROW(x)
     if (!is.matrix(x) & !is.data.frame(x)) {
       ret <- c(x)
-      if (is.factor(ret) && length(levels(ret)) > 2)
+      if (is.factor(ret) && length(levels(ret)) > 2) {
         stop("Only binary factors are allowed for Y.")
+      }
       return(ret)
     } else {
-      if (NCOL(x) > 1 && test == "pcm")
+      if (NCOL(x) > 1 && test == "pcm") {
         stop("Please provide Y as a vector.")
+      }
       .check_data(x, mode = "X")
     }
   }
-  if ("tibble" %in% class(x))
+  if ("tibble" %in% class(x)) {
     x <- as.data.frame(x)
-  if (NCOL(x) == 1)
+  }
+  if (NCOL(x) == 1) {
     x <- as.matrix(x)
-  if (is.null(colnames(x)))
+  }
+  if (is.null(colnames(x))) {
     colnames(x) <- paste0(mode, seq_len(NCOL(x)))
+  }
   x
 }
 
 #' @importFrom stats pnorm
 .gcm <- function(
-    rY, rX, alternative = "two.sided", type = "quadratic", B = 499L
-) {
+    rY, rX, alternative = "two.sided", type = "quadratic", B = 499L) {
   dY <- NCOL(rY)
   dX <- NCOL(rX)
   nn <- NROW(rY)
   RR <- rY * rX
   if (dY > 1 || dX > 1) {
     if (type == "quadratic") {
-      sigma <- crossprod(RR)/nn - tcrossprod(colMeans(RR))
+      sigma <- crossprod(RR) / nn - tcrossprod(colMeans(RR))
       eig <- eigen(sigma)
-      if (min(eig$values) < .Machine$double.eps)
+      if (min(eig$values) < .Machine$double.eps) {
         warning("`vcov` of test statistic is not invertible")
-      siginvhalf <- eig$vectors %*% diag(eig$values^(-1/2)) %*%
+      }
+      siginvhalf <- eig$vectors %*% diag(eig$values^(-1 / 2)) %*%
         t(eig$vectors)
       tstat <- siginvhalf %*% colSums(RR) / sqrt(nn)
       stat <- sum(tstat^2)
@@ -197,19 +206,18 @@ gcm <- function(
     } else {
       tRR <- t(RR)
       mRR <- rowMeans(tRR)
-      tRR <- tRR/sqrt((rowMeans(tRR^2) - mRR^2))
+      tRR <- tRR / sqrt((rowMeans(tRR^2) - mRR^2))
       stat <- max(abs(mRR)) * sqrt(nn)
       sim <- apply(abs(tRR %*% matrix(
-        stats::rnorm(nn * B), nn, B)), 2, max) / sqrt(nn)
-      pval <- (sum(sim >= stat) + 1)/(B + 1)
+        stats::rnorm(nn * B), nn, B
+      )), 2, max) / sqrt(nn)
+      pval <- (sum(sim >= stat) + 1) / (B + 1)
     }
-  }
-  else {
+  } else {
     R.sq <- RR^2
     meanR <- mean(RR)
     stat <- sqrt(nn) * meanR / sqrt(mean(R.sq) - meanR^2)
-    pval <- switch(
-      alternative,
+    pval <- switch(alternative,
       "two.sided" = 2 * stats::pnorm(-abs(stat)),
       "greater" = 1 - stats::pnorm(stat),
       "less" = stats::pnorm(stat)
@@ -220,15 +228,17 @@ gcm <- function(
 }
 
 .rm_int <- function(x) {
-  if (all(x[, 1] == 1))
+  if (all(x[, 1] == 1)) {
     return(x[, -1L, drop = FALSE])
+  }
   x
 }
 
 #' @importFrom stats terms
 .get_terms <- function(formula) {
-  if (is.null(formula))
+  if (is.null(formula)) {
     return(NULL)
+  }
   atms <- stats::terms(formula)
   tms <- attr(atms, "term.labels")
   resp <- all.vars(formula)[1]
@@ -236,8 +246,10 @@ gcm <- function(
   tms[ridx] <- paste0("(", tms[ridx], ")")
   ie <- grep(":", tms, value = TRUE)
   me <- grep(":", tms, value = TRUE, invert = TRUE)
-  list(all = tms, me = me, ie = ie, response = resp, terms = atms,
-       fml = formula)
+  list(
+    all = tms, me = me, ie = ie, response = resp, terms = atms,
+    fml = formula
+  )
 }
 
 # Ranger ------------------------------------------------------------------
@@ -247,17 +259,25 @@ gcm <- function(
   response <- stats::model.response(stats::model.frame(formula, data))
   is_factor <- is.factor(response)
   tms <- .get_terms(formula)
-  resp <- if (is_factor)
-    .rm_int(stats::model.matrix(~ response, contrasts.arg = list(
-      "response" = "contr.treatment")))
-  else response
+  resp <- if (is_factor) {
+    .rm_int(stats::model.matrix(~response, contrasts.arg = list(
+      "response" = "contr.treatment"
+    )))
+  } else {
+    response
+  }
   tmp <- list(data = data, response = resp, is_factor = is_factor)
   if (identical(tms$me, character(0))) {
-    if (is_factor)
+    if (is_factor) {
       return(structure(c(list(mean = base::colMeans(resp)), tmp),
-                       class = "ranger"))
-    else return(structure(c(list(mean = mean(as.numeric(response))),
-                            tmp), class = "ranger"))
+        class = "ranger"
+      ))
+    } else {
+      return(structure(c(
+        list(mean = mean(as.numeric(response))),
+        tmp
+      ), class = "ranger"))
+    }
   }
   ret <- ranger::ranger(formula, data, probability = is_factor, ...)
   structure(c(ret, tmp), class = "ranger")
@@ -265,20 +285,28 @@ gcm <- function(
 
 #' @importFrom stats predict
 residuals.ranger <- function(object, newdata = NULL, newy = NULL, ...) {
-  if (is.null(newdata))
+  if (is.null(newdata)) {
     newdata <- object$data
-  if (!is.null(newy))
-    newy <- if (object$is_factor)
-      .rm_int(stats::model.matrix(~ newy, contrasts.arg = list(
-        "newy" = "contr.treatment")))
-  else newy
-  if (is.null(newy))
+  }
+  if (!is.null(newy)) {
+    newy <- if (object$is_factor) {
+      .rm_int(stats::model.matrix(~newy, contrasts.arg = list(
+        "newy" = "contr.treatment"
+      )))
+    } else {
+      newy
+    }
+  }
+  if (is.null(newy)) {
     newy <- object$response
-  if (!is.null(object$mean))
+  }
+  if (!is.null(object$mean)) {
     return(newy - object$mean)
+  }
   preds <- stats::predict(object, data = newdata)$predictions
-  if (object$is_factor)
+  if (object$is_factor) {
     preds <- preds[, -1]
+  }
   unname(newy - preds)
 }
 
@@ -296,15 +324,20 @@ residuals.ranger <- function(object, newdata = NULL, newy = NULL, ...) {
 plot.gcm <- function(x, plot = TRUE, ...) {
   .data <- NULL
   pd <- tidyr::pivot_longer(data.frame(rY = unname(x$rY), rX = unname(x$rX)),
-                            dplyr::starts_with("rX"), names_to = "nX",
-                            values_to = "rX")
+    dplyr::starts_with("rX"),
+    names_to = "nX",
+    values_to = "rX"
+  )
   if (NCOL(x$rY > 1)) {
     pd <- tidyr::pivot_longer(pd, dplyr::starts_with("rY"),
-                              names_to = "nY", values_to = "rY")
-  } else pd$nY <- "rY.1"
+      names_to = "nY", values_to = "rY"
+    )
+  } else {
+    pd$nY <- "rY.1"
+  }
   if (requireNamespace("ggplot2")) {
     p1 <- ggplot2::ggplot(pd, ggplot2::aes(
-      x = .data[["rX"]] , y = .data[["rY"]],
+      x = .data[["rX"]], y = .data[["rY"]],
       color = interaction(.data[["nY"]], .data[["nX"]]),
       linetype = .data[["nY"]]
     )) +
@@ -317,5 +350,63 @@ plot.gcm <- function(x, plot = TRUE, ...) {
   return(invisible(p1))
 }
 
-.mm <- function(preds, data)
+.mm <- function(preds, data) {
   .rm_int(stats::model.matrix(stats::reformulate(preds), data = data))
+}
+
+#' GCM test with pre-computed residuals
+#'
+#' @param rY Vector or matrix of response values.
+#' @param rX Matrix or data.frame of covariates.
+#' @param type Type of test statistic, either \code{"quadratic"} (default) or
+#'     \code{"max"}. If \code{"max"} is specified, the p-value is computed
+#'     based on a bootstrap approximation of the null distribution with
+#'     \code{B} samples.
+#' @param alternative A character string specifying the alternative hypothesis,
+#'     must be one of \code{"two.sided"} (default), \code{"greater"} or
+#'     \code{"less"}. Only applies if \code{type = "quadratic"} and \code{Y} and
+#'     \code{X} are one-dimensional.
+#' @param ... Further arguments passed to \code{\link[coin]{independence_test}()}.
+#'
+#' @returns Object of class '\code{gcm}' and '\code{htest}' with the following
+#' components:
+#' \item{\code{statistic}}{The value of the test statistic.}
+#' \item{\code{p.value}}{The p-value for the \code{hypothesis}}
+#' \item{\code{parameter}}{In case X is multidimensional, this is the degrees of
+#'     freedom used for the chi-squared test.}
+#' \item{\code{hypothesis}}{String specifying the null hypothesis.}
+#' \item{\code{null.value}}{String specifying the null hypothesis.}
+#' \item{\code{method}}{The string \code{"Generalised covariance measure test"}.}
+#' \item{\code{data.name}}{A character string giving the name(s) of the data.}
+#' \item{\code{rY}}{Residuals for the Y on Z regression.}
+#' \item{\code{rX}}{Residuals for the X on Z regression.}
+#'
+rgcm <- function(
+    rY, rX, alternative = "two.sided",
+    type = c("quadratic", "max", "scalar"), ...) {
+  type <- match.arg(type)
+  tst <- coin::independence_test(rY ~ rX,
+    alternative = alternative, teststat = type, ...
+  )
+  df <- NULL
+  switch(type,
+    "scalar" = {
+      stat <- c("Z" = coin::statistic(tst))
+    },
+    "quadratic" = {
+      stat <- c("X-squared" = coin::statistic(tst))
+      df <- tst@statistic@df
+    },
+    "max" = {
+      stat <- c("maxT" = coin::statistic(tst))
+    }
+  )
+  structure(list(
+    statistic = stat, p.value = coin::pvalue(tst), parameter = df,
+    hypothesis = c("E[cov(Y, X | Z)]" = "0"),
+    null.value = c("E[cov(Y, X | Z)]" = "0"), alternative = alternative,
+    method = paste0("Generalized covariance measure test"),
+    data.name = deparse(match.call(), width.cutoff = 80),
+    rY = rY, rX = rX
+  ), class = c("gcm", "htest"))
+}
